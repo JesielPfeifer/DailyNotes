c84ad5d27e517a4f4abff418fdcda6dc922a5f2f
jesiel_pfeifer <jesiel.pfeifer@gmail.com>
2025-01-19 15:55:23 -0300

    finished eslint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,12 +8,25 @@ module.exports = [
     languageOptions: {
       ecmaVersion: 'latest', // Suporte à última versão do ECMAScript
       sourceType: 'module', // Habilitar módulos ES
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Habilitar JSX
+        },
+      },
     },
     plugins: {
-      prettier: eslintPluginPrettier, // Adiciona o plugin Prettier
+      prettier: eslintPluginPrettier, // Adicionar o plugin Prettier
+      react: require('eslint-plugin-react'), // Adicionar o plugin React
+    },
+    settings: {
+      react: {
+        version: 'detect', // Detectar automaticamente a versão do React
+      },
     },
     rules: {
       'prettier/prettier': 'error', // Configura a regra do Prettier
+      'react/jsx-uses-react': 'error', // Evitar remoção incorreta de imports React
+      'react/jsx-uses-vars': 'error', // Garantir que variáveis JSX são usadas
     },
   },
 ];

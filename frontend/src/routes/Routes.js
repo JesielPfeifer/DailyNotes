c461b0f5b4b6aca8af201dbe3524e66d05775477
jesiel_pfeifer <jesiel.pfeifer@gmail.com>
2025-01-20 20:11:16 -0300

    Working on public and Private Routes

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Register from '../components/Register/Register';
+import Login from '../components/Login/Login';
+import App from '../App';
+import PrivateRoute from '../services/PrivateRoutes';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
-      {/* <Route path="/register" element={<Register />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/app" element={<App />} />
+      </Route>
     </Routes>
   );
 };
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken'); // Obtém o token do Local Storage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Adiciona o token ao cabeçalho
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default api;
